5d32af4cf7831265e9c069c5dbc1a2b01edcbdb0
julia.vakulina <yuliavak@mail.ru>
2024-10-04 20:28:49 +0700

    Display Vacancy Detail

--- favorites/src/main/java/ru/practicum/android/diploma/favorites/data/repositoryimpl/db/FavoriteRepositoryImpl.kt
+++ favorites/src/main/java/ru/practicum/android/diploma/favorites/data/repositoryimpl/db/FavoriteRepositoryImpl.kt
@@ -1,10 +1,14 @@
 package ru.practicum.android.diploma.favorites.data.repositoryimpl.db
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.commonutils.Resource
 import ru.practicum.android.diploma.commonutils.Utils
 import ru.practicum.android.diploma.data.db.AppDatabase
+import ru.practicum.android.diploma.data.db.entity.VacancyEntity
 import ru.practicum.android.diploma.favorites.data.mappers.VacancyDbMapper
 import ru.practicum.android.diploma.favorites.domain.model.FavoriteVacancy
 import ru.practicum.android.diploma.favorites.domain.repository.FavoriteRepository
@@ -14,6 +18,36 @@ private const val TAG_VACANCIES_PAGINATED = "VacanciesPaginated"
 class FavoriteRepositoryImpl(
     private val dataBase: AppDatabase
 ) : FavoriteRepository {
+
+    init {
+        addMockToDb()
+    }
+
+    private fun addMockToDb() {
+        GlobalScope.launch(Dispatchers.IO) {
+            val name = "name"
+            val nameCompany = "nameCompany"
+            val location = "location"
+            val experience = "experience"
+            val employment = "employment"
+            val description = "description"
+            for (i in 0..123) {
+                dataBase.favoriteVacancyDao().insertOrUpdateVacancy(
+                    VacancyEntity(
+                        idVacancy = i,
+                        nameVacancy = "${name}_${i}",
+                        salary = "${i} 000",
+                        nameCompany = "${nameCompany}_${i}",
+                        location = "${location}_${i}",
+                        experience = "${experience}_${i}",
+                        employment = "${employment}_${i}",
+                        description = "${description}_${i}",
+                        urlLogo = ""
+                    )
+                )
+            }
+        }
+    }
 
     override suspend fun getVacancies(): Flow<List<FavoriteVacancy>> = flow {
         emit(
--- vacancy/src/main/java/ru/practicum/android/diploma/vacancy/presentation/ui/VacancyFragment.kt
+++ vacancy/src/main/java/ru/practicum/android/diploma/vacancy/presentation/ui/VacancyFragment.kt
@@ -2,16 +2,25 @@ package ru.practicum.android.diploma.vacancy.presentation.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
+import ru.practicum.android.diploma.commonutils.Utils
+import ru.practicum.android.diploma.data.db.dao.VacancyDao_Impl
+import ru.practicum.android.diploma.ui.R
 import ru.practicum.android.diploma.vacancy.databinding.FragmentVacancyBinding
 import ru.practicum.android.diploma.vacancy.presentation.ui.state.VacancyInputState
 import ru.practicum.android.diploma.vacancy.presentation.viewmodel.VacancyDetailViewModel
+import ru.practicum.android.diploma.vacancy.presentation.viewmodel.state.VacancyDetailState
 
 class VacancyFragment : Fragment() {
 
@@ -19,6 +28,9 @@ class VacancyFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val vacancyDetailViewModel: VacancyDetailViewModel by viewModel()
+    private var idDb: Int = 0
+    private var idNetwork: String = ""
+    private var argsState: Int = 2
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,11 +57,108 @@ class VacancyFragment : Fragment() {
                 context?.startActivity(this)
             }
         }
+
+        argsState = requireArguments().getInt(ARGS_STATE)
+        idDb = requireArguments().getInt(VACANCY_ID_DB)
+        idNetwork = requireArguments().getString(VACANCY_ID_NETWORK).toString()
+
+        if (argsState == INPUT_NETWORK_STATE) {
+            vacancyDetailViewModel.showVacancyNetwork(idNetwork)
+        }
+        if (argsState == INPUT_DB_STATE) {
+            vacancyDetailViewModel.showVacancyDb((idDb))
+        }
+
+        vacancyDetailViewModel.observeVacancyState().observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is VacancyDetailState.Loading -> {
+                    showLoading()
+                }
+
+                is VacancyDetailState.Content -> {
+                    showContent(
+                        state.vacancy.nameVacancy,
+                        state.vacancy.salary,
+                        state.vacancy.urlLogo,
+                        state.vacancy.nameCompany,
+                        state.vacancy.location,
+                        state.vacancy.experience,
+                        state.vacancy.employment,
+                        state.vacancy.description,
+                        "key skills"
+                    )
+                }
+
+                is VacancyDetailState.Error -> {
+                    showError()
+                }
+
+                is VacancyDetailState.Empty -> {
+                    showEmpty()
+                }
+            }
+
+        }
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun showLoading() {
+        binding.vacancyNotFoundError.visibility = View.GONE
+        binding.vacancyServerError.visibility = View.GONE
+        binding.progressbar.visibility = View.VISIBLE
+    }
+
+    private fun showContent(name: String, salary: String, urlLogo: String?,
+                            company: String, city: String, experience: String,
+                            conditions: String, description: String, keys: String) {
+        binding.progressbar.visibility = View.GONE
+        binding.vacancyServerError.visibility = View.GONE
+        binding.progressbar.visibility = View.GONE
+        binding.vacancyName.text = name
+        binding.vacancySalary.text = salary
+        if (urlLogo != null) {
+            Glide.with(this)
+                .load(urlLogo)
+                .placeholder(R.drawable.placeholder_logo_item_favorite)
+                .transform(
+                    CenterCrop(),
+                    context?.let {
+                        Utils.doToPx(
+                            RADIUS_ROUND_VIEW,
+                            it
+                        )
+                    }?.let {
+                        RoundedCorners(
+                            it
+                        )
+                    }
+                )
+                .transform()
+                .into(binding.vacancyImage)
+        }
+        binding.vacancyCompany.text = company
+        binding.vacancyCity.text = city
+        binding.vacancyExperienceInfo.text = experience
+        binding.vacancyConditions.text = conditions
+        binding.vacancyDescriptionInfo.text = description
+        binding.vacancyKeySkillsInfo.text = keys
+    }
+
+    private fun showError() {
+        binding.progressbar.visibility = View.GONE
+        binding.vacancyNotFoundError.visibility = View.GONE
+        binding.vacancyServerError.visibility = View.VISIBLE
+    }
+
+    private fun showEmpty() {
+        binding.progressbar.visibility = View.GONE
+        binding.vacancyServerError.visibility = View.GONE
+        binding.vacancyNotFoundError.visibility = View.VISIBLE
     }
 
     companion object {
@@ -61,6 +170,8 @@ class VacancyFragment : Fragment() {
         private const val INPUT_NETWORK_STATE = 0
         private const val INPUT_DB_STATE = 1
 
+        private const val RADIUS_ROUND_VIEW = 12f
+
         fun createArgs(vacancyInputState: VacancyInputState): Bundle {
             return when (vacancyInputState) {
                 is VacancyInputState.VacancyNetwork -> {
@@ -69,6 +180,7 @@ class VacancyFragment : Fragment() {
                         VACANCY_ID_NETWORK to vacancyInputState.id
                     )
                 }
+
                 is VacancyInputState.VacancyDb -> {
                     bundleOf(
                         ARGS_STATE to INPUT_DB_STATE,

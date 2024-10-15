d266955b1a4fb141ffcacf192dc375f754e6b918
Захаров Павел <pzakharov83@gmail.com>
2024-10-15 23:38:03 +0300

    last fix

--- feature-filter/src/main/java/ru/practicum/android/diploma/filter/place/presentation/ui/PlaceFragment.kt
+++ feature-filter/src/main/java/ru/practicum/android/diploma/filter/place/presentation/ui/PlaceFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -141,6 +140,7 @@ internal class PlaceFragment : Fragment() {
                 regionsCountriesViewModel.clearCache()
                 findNavController().navigateUp()
             }
+
             R.id.buttonBack -> {
                 regionsCountriesViewModel.mergeSettingsWithBufferDataInSp()
                 regionsCountriesViewModel.clearCache()
